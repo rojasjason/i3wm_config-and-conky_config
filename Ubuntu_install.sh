849805d8a61eb06d8b5111b883c6e6f25ccb2ace
rojasjason <jasonrojasemail@gmail.com>
2017-07-01 23:37:10 -0500

    Update Ubuntu_install.sh

--- Ubuntu_install.sh
+++ Ubuntu_install.sh
@@ -9,7 +9,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 clear
 
 # Install all programs (in repos)
-sudo apt-get install i3 feh lxappearance ranger gnome-icon-theme-full compton
+sudo apt-get install i3 feh lxappearance ranger gnome-icon-theme-full compton i3blocks
 
 # Playctl
 sudo dpkg -i playerctl-0.5.0_amd64.deb
