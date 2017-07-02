3f94b9aa948c4c14d9e88ff01925e1e3f73ffeb7
rojasjason <jasonrojasemail@gmail.com>
2017-07-01 21:35:46 -0500

    Update Ubuntu_install.sh

--- Ubuntu_install.sh
+++ Ubuntu_install.sh
@@ -8,7 +8,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 clear
 
 # Install all programs (in repos)
-sudo apt-get install i3 feh lxappearance
+sudo apt-get install i3 feh lxappearance ranger gnome-icon-theme-full
 
 # Playctl
 sudo dpkg -i playerctl-0.5.0_amd64.deb
@@ -17,7 +17,7 @@ sudo dpkg -i playerctl-0.5.0_amd64.deb
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
-sudo apt-get install spotify-client
+sudo apt-get install spotify-client -y
 
 # Font-Awesome
 mkdir ~/.fonts
@@ -28,6 +28,19 @@ mv VeraMono.ttf ~/.fonts/
 mv VeraMoIt.ttf ~/.fonts/
 mv VeraMoBd.ttf ~/.fonts/
 mv VeraMoBI.ttf ~/.fonts/
+
+# Arc Theme
+wget -nv http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key -O Release.key
+sudo apt-key add - < Release.key
+sudo apt-get update
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/arc-theme.list"
+sudo apt-get update
+sudo apt-get install arc-theme -y
+
+#Moke Icon Theme
+sudo add-apt-repository ppa:moka/daily
+sudo apt-get update
+sudo apt-get install moka-icon-theme faba-icon-theme faba-mono-icons -y
 
 # Logout
 echo "See you on the other side and thank you!"
