3d783b1daeb13252100e790f27dcc7e28d168428
rojasjason <jasonrojasemail@gmail.com>
2017-07-02 01:27:52 -0500

    Update Ubuntu_install.sh

--- Ubuntu_install.sh
+++ Ubuntu_install.sh
@@ -54,6 +54,9 @@ sudo add-apt-repository ppa:dawidd0811/neofetch
 sudo apt update
 sudo apt install neofetch -y
 
+# Atom Install
+sudo dpkg --install atom-amd64.deb -y
+
 # i3 Gaps Repo Add (Last Step)
 
 # Make the folder
